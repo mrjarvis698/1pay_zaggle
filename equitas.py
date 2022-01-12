fc4b2d2b19c9d179d7810027cbdf7b70beced64a
mrjarvis698 <dudamajaykumar59@gmail.com>
2022-01-12 05:42:16 +0530

    1pay: selenium actions for equitas card payments
    Signed-off-by: mrjarvis698 <dudamajaykumar59@gmail.com>

--- equitas.py
+++ equitas.py
@@ -8,6 +8,7 @@ from openpyxl import load_workbook
 from openpyxl.workbook import Workbook
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import time
 
 # Open xlsx file
 open_sheet = path.exists("cache/opened_sheet.json")
@@ -73,6 +74,8 @@ if output_sheet == True :
   output_sheet_file_path = "Output.xlsx"
 else :
   input_col.append('No.of Transactions')
+  input_col.append('Status')
+  input_col.append('PG Transaction Number')
   output_headers = input_col
   overall_output = Workbook()
   page = overall_output.active
@@ -81,8 +84,8 @@ else :
   output_sheet_file_path = "Output.xlsx"
 
 def output_save():
-  global output_wb
-  entry_list = [[input_xlsx_col_A[x], input_xlsx_col_B[x], input_xlsx_col_C[x], input_xlsx_col_D[x], input_xlsx_col_E[x], input_xlsx_col_F[x], input_xlsx_col_G[x], input_xlsx_col_H[x], input_xlsx_col_I[x], input_xlsx_col_J[x], z + 1]]
+  global output_wb, entry_list
+  entry_list = [[input_xlsx_col_A[x], input_xlsx_col_B[x], input_xlsx_col_C[x], input_xlsx_col_D[x], input_xlsx_col_E[x], input_xlsx_col_F[x], input_xlsx_col_G[x], input_xlsx_col_H[x], input_xlsx_col_I[x], input_xlsx_col_J[x], z + 1, transaction_status, pg_transaction_reference_number]]
   output_wb = load_workbook(output_sheet_file_path)
   page = output_wb.active
   for info in entry_list:
@@ -114,17 +117,71 @@ caps["pageLoadStrategy"] = "normal"
 driver=webdriver.Chrome(chrome_options=chrome_options, desired_capabilities=caps, executable_path="chromedriver.exe")
 driver.maximize_window()
 
+def start_link():
+  driver.get("https://pay.1paypg.in/onepayVAS/earn-more156")
+
+def main():
+  global transaction_status, pg_transaction_reference_number
+  driver.find_element_by_id("custMobile").click()
+  driver.find_element_by_id("custMobile").clear()
+  driver.find_element_by_id("custMobile").send_keys(input_xlsx_col_C[x])
+  driver.find_element_by_id("udf1").click()
+  driver.find_element_by_id("udf1").clear()
+  driver.find_element_by_id("udf1").send_keys(input_xlsx_col_A[x] + input_xlsx_col_B[x])
+  driver.find_element_by_id("custMail").click()
+  driver.find_element_by_id("custMail").clear()
+  driver.find_element_by_id("custMail").send_keys("test@test.com")
+  driver.find_element_by_id("subMe").click()
+  time.sleep(2)
+  driver.find_element_by_xpath('//*[@id="tab-menu"]/li/label').click()
+  driver.find_element_by_xpath("//div[@id='credit-card-info']/div[4]/div/label").click()
+  driver.find_element_by_id("card-number").click()
+  driver.find_element_by_id("card-number").send_keys(input_xlsx_col_E[x])
+  driver.find_element_by_id("card-holder").click()
+  driver.find_element_by_id("card-holder").clear()
+  driver.find_element_by_id("card-holder").send_keys(input_xlsx_col_A[x] + input_xlsx_col_B[x])
+  driver.find_element_by_id("card-month").click()
+  driver.find_element_by_id("card-month").clear()
+  driver.find_element_by_id("card-month").send_keys("04")
+  driver.find_element_by_id("card-year").click()
+  driver.find_element_by_id("card-year").clear()
+  driver.find_element_by_id("card-year").send_keys("2023")
+  driver.find_element_by_id("card-cvc").click()
+  driver.find_element_by_id("card-cvc").clear()
+  driver.find_element_by_id("card-cvc").send_keys(input_xlsx_col_G[x])
+  driver.find_element_by_id("payNowCC1").click()
+  time.sleep(2)
+  driver.find_element_by_xpath('//*[@id="tab-B-label"]').click()
+  driver.find_element_by_id("expDate").click()
+  driver.find_element_by_id("expDate").clear()
+  driver.find_element_by_id("expDate").send_keys("04-2023")
+  driver.find_element_by_id("pin").click()
+  driver.find_element_by_id("pin").clear()
+  driver.find_element_by_id("pin").send_keys(input_xlsx_col_F[x])
+  driver.find_element_by_xpath("//button[@id='submitButtonIdForPin']/b").click()
+  time.sleep(2)
+  transaction_status = driver.find_element_by_xpath("/html/body/center/h1").text
+  pg_transaction_reference_number = driver.find_element_by_xpath("/html/body/table[2]/tbody/tr[5]/td[2]").text
+
 try:
   cal()
 except IndexError:
   for x in range (0 , total_input_rows):
     for z in range (0, 5):
-        output_save()
+      start_link()
+      main()
+      output_save()
+      print (entry_list)
+      time.sleep(0.5)
 else:
   last_txncard =  input_workbook[input_workbook[input_col[4]] == output_cc_number[h]].index[0]
   for x in range (last_txncard , total_input_rows):
     for z in range (int(done_transactions_wb[h]), 5):
-        output_save()
+      start_link()
+      main()
+      output_save()
+      print (entry_list)
+      time.sleep(0.5)
     done_transactions_wb[h] = 0
 
 driver.quit()
