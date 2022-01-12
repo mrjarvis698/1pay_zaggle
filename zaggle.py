57b1c7a963e0b2686b8d390f79f680b7d923b460
mrjarvis698 <dudamajaykumar59@gmail.com>
2022-01-12 08:41:13 +0530

    1pay: headless: turnoff gay logging of chrome
    Signed-off-by: mrjarvis698 <dudamajaykumar59@gmail.com>

--- zaggle.py
+++ zaggle.py
@@ -118,6 +118,15 @@ chrome_options = webdriver.ChromeOptions()
 #chrome_options.add_argument("--incognito")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--window-size=1920,1080")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-crash-reporter")
+chrome_options.add_argument("--disable-extensions")
+chrome_options.add_argument("--disable-in-process-stack-traces")
+chrome_options.add_argument("--disable-logging")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--log-level=3")
+chrome_options.add_argument("--output=/dev/null")
 caps = DesiredCapabilities().CHROME
 #caps["pageLoadStrategy"] = "none"
 #caps["pageLoadStrategy"] = "eager"
